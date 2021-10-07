9fc44d30c4c4fc0587e71506153a786bb4e32ed3
Jack <j-bass@outlook.com>
2021-10-07 18:18:21 +0100

    Added user profiles with profile image

--- plant_tracker/settings.py
+++ plant_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from decouple import config
 
@@ -125,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- plant_tracker/urls.py
+++ plant_tracker/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from users import views as user_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +16,6 @@ urlpatterns = [
 
     path('', include('plants.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
