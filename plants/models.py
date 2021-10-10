f58ccb1eaeb3c891d85866f61496095ff1e10013
Jack <j-bass@outlook.com>
2021-10-10 15:27:34 +0100

    Added update user profile

--- plants/models.py
+++ plants/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Plant(models.Model):
@@ -9,5 +10,20 @@ class Plant(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default-plant.png', upload_to='plant-pics')
 
+    # TODO:
+    # watered = list of dates watered
+    # notes = list of text entries
+    #
+
     def __str__(self):
         return self.name
+
+    def save(self):
+        super().save()  # run the parent class' save method
+
+        img = Image.open(self.image.path)
+
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
