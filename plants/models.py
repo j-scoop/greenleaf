a9ce4569221dc177ce113cfbbf94afa085742fc5
Jack <j-bass@outlook.com>
2021-10-10 14:24:24 +0100

    Added images to plant model

--- plants/models.py
+++ plants/models.py
@@ -7,6 +7,7 @@ class Plant(models.Model):
     name = models.CharField(max_length=50)
     date_added = models.DateTimeField(default=timezone.now)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default-plant.png', upload_to='plant-pics')
 
     def __str__(self):
         return self.name
